0fbcf38d482385c4bbee96d73bc2ea23bf6741b5
bitcoder-17 <jsliang@uark.edu>
2020-10-23 18:28:20 -0500

    kinda sus

--- src/sidebar.js
+++ src/sidebar.js
@@ -7,10 +7,13 @@ function Sidebar() {
       <p>
 
         Sidebar.js is loading properly. Now Initializing...
-        Something at Line 10
+        Something at Line 10, hELLO THIS LINE IS sus
         Sidebar.js is loading properly. Reet
-
+        kinda sus as well
       </p>  
+      <h1>
+          Hello sus!
+      </h1>
     </>
   );
 }
