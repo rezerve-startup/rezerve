8e7c8da452e412824d06ca0a49e7b9086b181b8b
Miguel Campbell <72178579+Migotaur@users.noreply.github.com>
2020-10-24 22:11:47 -0500

    Testing 1

--- src/sidebar.js
+++ src/sidebar.js
@@ -4,16 +4,10 @@ import './sidebar.css'
 function Sidebar() {
   return (
     <>
-      <p>
+      
 
-        Sidebar.js is loading properly. Now Initializing...
-        Something at Line 10, hELLO THIS LINE IS sus
-        Sidebar.js is loading properly. Reet
-        kinda sus as well
-      </p>  
-      <h1>
-          Hello sus!
-      </h1>
+       
+      
     </>
   );
 }
