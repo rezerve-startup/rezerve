24f66bd8ef8d0d75a617a8451293407f43d16dd1
bitcoder-17 <jsliang@uark.edu>
2020-10-24 22:04:47 -0500

    First Version

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
+import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import { sidebarData } from './sidebarData' 
 import './sidebar.css'
+import { IconContext } from 'react-icons'
+import Typography from '@material-ui/core/Typography'
+
+
 
 function Sidebar() {
-  return (
-    <>
-      <p>
 
-        Sidebar.js is loading properly. Now Initializing...
-        Something at Line 10, hELLO THIS LINE IS sus
-        Sidebar.js is loading properly. Reet
-        kinda sus as well
-      </p>  
-      <h1>
-          Hello sus!
-      </h1>
-    </>
-  );
+    const [sidebar, setSidebar] = useState(false)
+    const showSidebar = () => setSidebar(!sidebar)
+
+    return (
+        <>
+        <IconContext.Provider value={{ color : '#fff' }}>
+        <div className='navbar'>
+            <Link to="#" classname='menu-bars'>
+                <FaIcons.FaBars onClick={showSidebar}/>
+            </Link>
+            <Typography variant="h4" component="h2" className="rezerve-head">
+                Rezerve
+            </Typography>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+            <ul className='nav-menu-items' onClick={showSidebar}>
+                <li className='navbar-toggle'>
+                    <Link to='#' className='menu-bars'>
+                        <AiIcons.AiOutlineClose />
+                    </Link>
+                </li>
+                {sidebarData.map((item, index) => {
+                    return (
+                        <li key={index} className={item.cName}>
+                           <Link to={item.path}>
+                                {item.icon}
+                                <span>{item.title}</span>   
+                            </Link> 
+                        </li>
+                    )
+                })}
+            </ul>
+        </nav>
+        </IconContext.Provider>
+        </>
+    );
 }
 
 export default Sidebar;
