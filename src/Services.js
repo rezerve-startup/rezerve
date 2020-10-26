d87032bb5ce32c214df0e3e6bd5596882d88c0ce
Brayden Alvarez <baalvare@uark.edu>
2020-10-26 01:49:38 -0500

    initial set up

---
+++ src/Services.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Services.css'
+
+class Services extends Component { 
+    render() {
+        return (
+            <div class="services-page">
+                <h1>Services Page</h1>
+            </div>
+        )
+    }
+}
+
+export default Services
