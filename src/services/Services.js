db25de6d7ee6e562509f8b40583add27ba6983a0
Brayden Alvarez <baalvare@uark.edu>
2020-10-26 15:23:10 -0500

    initial card setup

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
 import './Services.css'
 
 class Services extends Component { 
@@ -6,6 +7,17 @@ class Services extends Component {
         return (
             <div class="services-page">
                 <h1>Services Page</h1>
+                <div class="card-container">
+                    <Card>
+                        <Card.Img src="assets/cat1.jpg" alt="Card image" />
+                        <Card.ImgOverlay>
+                            <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.ImgOverlay>
+                    </Card>
+                </div>
             </div>
         )
     }
