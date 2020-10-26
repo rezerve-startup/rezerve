5a310868f74ab974d02aa44a7ebcad30b44450f3
tdgerth <tgerth4@gmail.com>
2020-10-26 17:19:53 -0500

    Updates

--- src/BusinessInfo.js
+++ src/BusinessInfo.js
@@ -5,39 +5,6 @@ import { Carousel, CarouselItem, CarouselCaption, CarouselControl, CarouselIndic
 // // import Carousel from 'react-responsive-carousel';
 import './BusinessInfo.css'
 
-// class BusinessInfo extends Component {
-//     render() {
-//         return (
-//             <div class="business-info-page">
-//                 <div class="business-overview">
-//                     <div class="carousel-container">
-//                         <Carousel class="image-carousel">
-//                             <Carousel.Item style={{"height": "300px"}} interval={3000}>
-//                                 <img style={{"height": "300px", "width": "500px"}} className="d-block mx-auto" src={"assets/cat1.jpg"} />
-//                             </Carousel.Item>
-//                             <Carousel.Item style={{"height": "300px"}} interval={3000}>
-//                                 <img style={{"height": "300px", "width": "500px"}} className="d-block mx-auto" src={"assets/cat2.jpg"} />
-//                             </Carousel.Item>
-//                             <Carousel.Item style={{"height": "300px"}} interval={3000}>
-//                                 <img style={{"height": "300px", "width": "500px"}} className="d-block mx-auto" src={"assets/cat3.jpg"} />
-//                             </Carousel.Item>
-//                         </Carousel>
-//                     </div>
-//                     <h5 class="business-name">Hello There</h5>
-//                     <h5>Business Name</h5>
-//                     <h5>Distance to Business</h5> 
-//                     <h5>Business Address</h5>
-//                 </div>
-//                 <div class="business-barbers">
-
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
-// export default BusinessInfo;
-
 
 const items = [
     {
@@ -48,7 +15,7 @@ const items = [
     {
       src: 'assets/cat2.jpg',
       altText: '',
-      caption: 'Freedom Isn\'t Free'
+      caption: ''
     },
     {
       src: 'assets/cat3.jpg',
