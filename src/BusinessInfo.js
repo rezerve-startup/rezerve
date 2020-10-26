0ee07bf90aa5544096a3b150878c43c37ed5d41a
tdgerth <tgerth4@gmail.com>
2020-10-26 16:19:44 -0500

    Made carousel responsive

--- src/BusinessInfo.js
+++ src/BusinessInfo.js
@@ -1,24 +1,128 @@
 import React, { Component } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import CarouselItem from 'react-bootstrap/CarouselItem';
+import { Carousel, CarouselItem, CarouselCaption, CarouselControl, CarouselIndicators } from 'reactstrap';
+// import Carousel from 'react-bootstrap/Carousel';
+// import CarouselItem from 'react-bootstrap/CarouselItem';
+// // import Carousel from 'react-responsive-carousel';
 import './BusinessInfo.css'
 
-class BusinessInfo extends Component {
+// class BusinessInfo extends Component {
+//     render() {
+//         return (
+//             <div class="business-info-page">
+//                 <div class="business-overview">
+//                     <div class="carousel-container">
+//                         <Carousel class="image-carousel">
+//                             <Carousel.Item style={{"height": "300px"}} interval={3000}>
+//                                 <img style={{"height": "300px", "width": "500px"}} className="d-block mx-auto" src={"assets/cat1.jpg"} />
+//                             </Carousel.Item>
+//                             <Carousel.Item style={{"height": "300px"}} interval={3000}>
+//                                 <img style={{"height": "300px", "width": "500px"}} className="d-block mx-auto" src={"assets/cat2.jpg"} />
+//                             </Carousel.Item>
+//                             <Carousel.Item style={{"height": "300px"}} interval={3000}>
+//                                 <img style={{"height": "300px", "width": "500px"}} className="d-block mx-auto" src={"assets/cat3.jpg"} />
+//                             </Carousel.Item>
+//                         </Carousel>
+//                     </div>
+//                     <h5 class="business-name">Hello There</h5>
+//                     <h5>Business Name</h5>
+//                     <h5>Distance to Business</h5> 
+//                     <h5>Business Address</h5>
+//                 </div>
+//                 <div class="business-barbers">
+
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default BusinessInfo;
+
+
+const items = [
+    {
+      src: 'assets/cat1.jpg',
+      altText: '',
+      caption: ''
+    },
+    {
+      src: 'assets/cat2.jpg',
+      altText: '',
+      caption: 'Freedom Isn\'t Free'
+    },
+    {
+      src: 'assets/cat3.jpg',
+      altText: '',
+      caption: ''
+    }
+  ];
+  
+  class BusinessInfo extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { activeIndex: 0 };
+      this.next = this.next.bind(this);
+      this.previous = this.previous.bind(this);
+      this.goToIndex = this.goToIndex.bind(this);
+      this.onExiting = this.onExiting.bind(this);
+      this.onExited = this.onExited.bind(this);
+    }
+  
+    onExiting() {
+      this.animating = true;
+    }
+  
+    onExited() {
+      this.animating = false;
+    }
+  
+    next() {
+      if (this.animating) return;
+      const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+      this.setState({ activeIndex: nextIndex });
+    }
+  
+    previous() {
+      if (this.animating) return;
+      const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+      this.setState({ activeIndex: nextIndex });
+    }
+  
+    goToIndex(newIndex) {
+      if (this.animating) return;
+      this.setState({ activeIndex: newIndex });
+    }
+  
     render() {
+      const { activeIndex } = this.state;
+  
+      const slides = items.map((item) => {
         return (
+          <CarouselItem
+            className="carouselImg"
+            onExiting={this.onExiting}
+            onExited={this.onExited}
+            key={item.src}
+          >
+            <img src={item.src} alt={item.altText} />
+            <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+          </CarouselItem>
+        );
+      });
+  
+      return (
             <div class="business-info-page">
                 <div class="business-overview">
                     <div class="carousel-container">
-                        <Carousel class="image-carousel">
-                            <Carousel.Item style={{"height": "300px"}} interval={3000}>
-                                <img style={{"height": "300px", "width": "500px"}} className="d-block mx-auto" src={"assets/cat1.jpg"} />
-                            </Carousel.Item>
-                            <Carousel.Item style={{"height": "300px"}} interval={3000}>
-                                <img style={{"height": "300px", "width": "500px"}} className="d-block mx-auto" src={"assets/cat2.jpg"} />
-                            </Carousel.Item>
-                            <Carousel.Item style={{"height": "300px"}} interval={3000}>
-                                <img style={{"height": "300px", "width": "500px"}} className="d-block mx-auto" src={"assets/cat3.jpg"} />
-                            </Carousel.Item>
+                        <Carousel
+                            activeIndex={activeIndex}
+                            next={this.next}
+                            previous={this.previous}
+                            >
+                            <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+                            {slides}
+                            <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                            <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
                         </Carousel>
                     </div>
                     <h5 class="business-name">Hello There</h5>
@@ -30,8 +134,9 @@ class BusinessInfo extends Component {
 
                 </div>
             </div>
-        )
+      );
     }
-}
-
-export default BusinessInfo;
+  }
+  
+  
+  export default BusinessInfo;
