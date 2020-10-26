365ce707ee03740264f9fcc68116af98382253ee
Dalynn Hatch <dalynn.hatch@outlook.com>
2020-10-26 15:54:25 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Services from './services/Services';
-import BusinessInfo from './BusinessInfo';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Services from "./services/Services";
+import BusinessInfo from "./BusinessInfo";
 
 function App() {
   return (
