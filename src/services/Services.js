cd464728c4d1140243bf3c449cc4a13fdd6932fa
Dalynn Hatch <dalynn.hatch@outlook.com>
2020-10-26 14:57:02 -0500

    formatting

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import Header from './header/Header';
-import './Services.css'
+import React, { Component } from "react";
+import Header from "./header/Header";
+import "./Services.css";
 
-class Services extends Component { 
-    render() {
-        return (
-            <div class="services-page">
-                <Header />
-                <h1>Services Page</h1>
-            </div>
-        )
-    }
+class Services extends Component {
+  render() {
+    return (
+      <div class="services-page">
+        <Header />
+        <h1>Services Page</h1>
+      </div>
+    );
+  }
 }
 
-export default Services
+export default Services;
--- src/services/header/Header.js
+++ src/services/header/Header.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import { FormControl, InputGroup, Nav } from 'react-bootstrap';
-import './Header.css'
+import React, { Component } from "react";
+import { FormControl, InputGroup, Nav } from "react-bootstrap";
+import "./Header.css";
 
-class Header extends Component { 
-    render() {
-        return (
-            <div class="header">
-                <div class="icons">
-                    <p>icons</p>
-                </div>
-                <Nav justify class="nav justify-content-center">
-                    <Nav.Item>
-                        <Nav.Link href="/forum">Forum</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/book">Book</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-                <InputGroup>
-                    <InputGroup.Prepend>
-                        <InputGroup.Text id="search">@</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="Search"
-                        aria-label="Search"
-                        aria-describedby="search"
-                    />
-                </InputGroup>
-            </div>
-        )
-    }
+class Header extends Component {
+  render() {
+    return (
+      <div class="header">
+        <div class="icons">
+          <p>icons</p>
+        </div>
+        <Nav justify class="nav justify-content-center">
+          <Nav.Item>
+            <Nav.Link href="/forum">Forum</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/book">Book</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <InputGroup>
+          <InputGroup.Prepend>
+            <InputGroup.Text id="search">@</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="search"
+          />
+        </InputGroup>
+      </div>
+    );
+  }
 }
 
-export default Header
+export default Header;
