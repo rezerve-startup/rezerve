ff11204c7ca6d0ada4a9640de666bb8fe61ac1cf
Miguel Campbell <72178579+Migotaur@users.noreply.github.com>
2020-10-30 15:47:24 -0500

    Added Text Fields

--- src/pagesRoute/profilePage.js
+++ src/pagesRoute/profilePage.js
@@ -1,29 +1,69 @@
 import React from 'react'
 import logo from '../images/avatar.jpg'
+import {Divider, TextField} from '@material-ui/core/'
 import '../user-profile.css'
 
 
 function profilePage() {
+    const infoList = [
+        {
+            stat : "Username",
+            current: "jbarber9000",
+            id: "filled-required"
+        },
+        {
+            stat : "Phone Number",
+            current: "479555555",
+            id: "standard-number"
+        },
+        {
+            stat : "Email Address",
+            current: "j.barb42@gmail.com",
+            id: "filled-required"
+        }
+
+    ];
+    
+    
+    
+    
     return (
         
-        
-        <div className="page_layout">
-           
-           <img src={logo} className="image" /> 
-           
+       
+        <div >
+           <div className="page_layout">
+           <h1>Edit Account</h1>
+           </div>
 
-           <div>
+           <div className="page_layout">
+           
+           <img src={logo} className="image"/> 
+           </div>
+
+           <div className="page_layout">
             <a href="#">Change</a>
             </div>
-        </div>
-        
-       
 
+            <div className="item_layout">
+            
+            {infoList.map((item, index) => {
+                    return (
+                        <div className="item_layout">
+                        <h2 key={index}>{item.stat}</h2>
+                        <TextField required id={item.id} defaultValue={item.current} />
+                        
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+
+        
   
         
         
         
-        
+      
     )
 }
 
