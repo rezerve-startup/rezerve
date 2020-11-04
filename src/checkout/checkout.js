c8775b38435a223b9b4963c20d246c8e1c93717e
moyeneka <60825726+moyeneka@users.noreply.github.com>
2020-11-04 02:49:02 -0600

    steve:basic structure

---
+++ src/checkout/checkout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../checkout/checkout.css'
+
+
+function Checkout() {
+    return(
+        <>
+            <p>Checkout Page Testing</p>
+        </>
+    );
+}
+
+
+
+
+export default Checkout
--- src/pagesRoute/PaymentInfo.js
+++ src/pagesRoute/PaymentInfo.js
@@ -2,12 +2,10 @@ import React from 'react'
 import Checkout from '../checkout/checkout'
 
 
-
-
 function PaymentInfo() {
     return (
         <div className='payments'>
-            {/*<h2>PaymentInfo Page</h2>*/}
+            
             <Checkout />
         </div>
     )
