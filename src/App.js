8368b98fe60fd565b1192774a8a0515f25dffbe7
Miguel Campbell <72178579+Migotaur@users.noreply.github.com>
2020-11-09 16:08:41 -0600

    Established App. Page for reference

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Help from './pagesRoute/Help'
 import Messages from './pagesRoute/Messages'
 import PaymentInfo from './pagesRoute/PaymentInfo'
 import Settings from './pagesRoute/Settings'
-import YouAppointment from './pagesRoute/YouAppointment'
+import AppointmentsPage from './pagesRoute/appointment/AppointmentsPage'
 import ProfilePage from './pagesRoute/profilePage/profilePage'
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
           <Route path="/messages" exact component={ Messages }/>
           <Route path="/payment" exact component={ PaymentInfo }/>
           <Route path="/settings" exact component={ Settings }/>
-          <Route path="/appointments" exact component={ YouAppointment }/>
+          <Route path="/appointments" exact component={ AppointmentsPage }/>
         </Switch>
       </Router>
     </>
---
+++ src/pagesRoute/appointment/AppointmentItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './AppointmentItem.css';
+
+
+class AppointmentItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        console.log(this.props);
+        return (
+            <div class="appointment-item">
+                <p class="business-picture"><i>Imagine there is a picture here</i></p>
+                <div class="appointment-overview">
+                    <p class="business-name">{this.props.businessName}</p>
+                    <p class="appointment-info">{this.props.appointmentDate}</p>
+                    <p class="appointment-info">{this.props.appointmentPrice}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default AppointmentItem;
