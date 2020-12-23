20dce72e3d4bb22f11ad3b51220e4f41eba46e5b
Miguel Campbell <72178579+Migotaur@users.noreply.github.com>
2020-12-23 11:17:00 -0600

    Checking out

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
+import {CustomerCreationPage} from './pagesRoute/CustomerCreationPage';
 
 
 function App() {
@@ -8,11 +9,12 @@ function App() {
   return (
     <>
       <Router>
-        <Sidebar />
+    
         <Switch>
-          <Route path="/help" exact component={ Help }/>
+          <Route path="/customer-signup" exact component={ CustomerCreationPage }/>
         </Switch>
       </Router>
+      <Link to="/customer-signup"><a>Customer Signup</a></Link>
     </>
   );
 
