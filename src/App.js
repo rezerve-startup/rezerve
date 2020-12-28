5cc8043a313d4e2f8dc1ee1679147b1880acdb56
Miguel Campbell <72178579+Migotaur@users.noreply.github.com>
2020-12-27 22:06:50 -0600

    Styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import basiclink from './basiclink'
 import './App.css';
-import CustomerCreationPage from './pagesRoute/CustomerCreationPage';
+import CustomerCreationPage from './pagesRoute/customer-signup/CustomerCreationPage';
 
 
 function App() {
 
   return (
     <>
+      <basiclink/>
+      
       <Router>
  
         <Switch>
           <Route path="/customer-signup" exact component={ CustomerCreationPage }/>
         </Switch>
-
-        <Link to="/customer-signup"><a>Customer Signup</a></Link>
+        
       </Router>
       
     </>
--- src/pagesRoute/customer-signup/CustomerCreationPage.js
+++ src/pagesRoute/customer-signup/CustomerCreationPage.js
@@ -1,29 +1,30 @@
 import React, {useState} from 'react'
-import logo from '../images/avatar.jpg'
+import logo from '../../images/avatar.jpg'
 import {ClickAwayListener, TextField} from '@material-ui/core/'
+import './customerSignup.css'
 
 
 function CustomerCreationPage(){
     return(
-<div>
-        <div> 
+<div ClassName="main">
+        <div ClassName="page-items"> 
             <h2>Name</h2>
             <TextField/>    
         </div>
 
-        <div> 
+        <div ClassName="page-items"> 
             <h2>Phone Number</h2>
             <TextField/>
             <a>Not now</a>    
         </div>
 
-        <div> 
+        <div ClassName="page-items"> 
             <h2>Email Address</h2>
             <TextField/>    
         </div>
 
 
-        <div> 
+        <div ClassName="page-items"> 
             <h2>Address/Location</h2>
             <TextField/>    
         </div>
