712178839c86bc5c27bdff9e4c3702c5d1566af1
moyeneka <60825726+moyeneka@users.noreply.github.com>
2020-12-31 10:16:26 -0600

    by micheal

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {Form, FormGroup} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import './CSS/BusinSignUp.css';
+import './CSS/Signup.css';
 
 function SignUpPage1(props) {
 
@@ -11,33 +12,32 @@ function SignUpPage1(props) {
 
     });  
 
-   
-		
-    const handleSubmit = event =>{
-        console.log("Current State is" + JSON.stringify(state))
-        event.preventDefault();
-    }
-
-
 
     return(
         <div className='signUp-page'>
-            <div className='container' id="SignUpPage">
-                <Form onSubmit={handleSubmit}>
+            <div className='container' /*id="SignUpPage"*/>
+                <Form /*onSubmit={handleSubmit}*/>
                     <div className="row col col-11 col-md-auto offset-1 offset-sm-0">
-                        <h1>Sign Up</h1>
+                        <h1>Sign&nbsp;Up</h1>
                     </div>
-                    
-						<FormGroup row>
-                        <div className="Next row col-11 offset-1 offset-sm-0">
-                            <Link to="/BusinSignUp">
-                                <button type="submit" className="btn btn-danger">
-                                    NEXT
-                                </button>
-                            </Link>
+                   <FormGroup row>
+                        <div className="row col-10 offset-2 offset-sm-3" >
+                            <label>Customer</label>
+                        </div>
+                    </FormGroup> 
+                    <FormGroup row>
+                        <div className="row col-10 offset-2 offset-sm-3" >
+                              <Link to = "/BusinSignUp">
+                                  <label className="labelContainer" >         
+                           
+                             Business
+                            
+                                </label>
+                                </Link>
                         </div>
                     </FormGroup>
-
+                    
+				
                 </Form>
             </div>
         </div>
