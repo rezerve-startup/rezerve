0b2475a227be0cf6153ff2f6474ea74db7d06336
moyeneka <60825726+moyeneka@users.noreply.github.com>
2021-01-02 03:31:32 -0600

    by micheal

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -15,31 +15,40 @@ function SignUpPage1(props) {
 
     return(
         <div className='signUp-page'>
-            <div className='container' /*id="SignUpPage"*/>
-                <Form /*onSubmit={handleSubmit}*/>
-                    <div className="row col col-11 col-md-auto offset-1 offset-sm-0">
-                        <h1>Sign&nbsp;Up</h1>
+            <Form /*onSubmit={handleSubmit}*/>
+                 <div className='container'>
+              
+                    <div className="row col col-11 col-md-auto offset-1 offset-sm-1">
+                        <h1><strong>Sign&nbsp;Up</strong></h1>
                     </div>
+                
                    <FormGroup row>
-                        <div className="row col-10 offset-2 offset-sm-3" >
-                            <label>Customer</label>
+                   <Link to = "/CustomSignUp">
+                        <div className="inputText row col-10 offset-2 offset-sm-0" >
+                            <label className ="labelContainer">
+                            <button class="button button1">Customer</button>
+                                </label>
                         </div>
+                        </Link>
                     </FormGroup> 
                     <FormGroup row>
-                        <div className="row col-10 offset-2 offset-sm-3" >
-                              <Link to = "/BusinSignUp">
-                                  <label className="labelContainer" >         
+                      <Link to = "/BusinSignUp">
+                          <div className="inputText row col-10 offset-2 offset-sm-6" >
+                              
+                                  <label  className ="labelContainer">         
                            
-                             Business
+                                  <button class="button button1">Business</button>
                             
                                 </label>
-                                </Link>
+                                
                         </div>
+                        </Link>
                     </FormGroup>
                     
-				
-                </Form>
-            </div>
+                 
+               
+            </div>  
+             </Form>
         </div>
     );
 }
