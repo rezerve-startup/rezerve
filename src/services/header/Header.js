621a530924d1d81d7414328e9665b71a3ab2eea6
EmrLan <57022651+EmrLan@users.noreply.github.com>
2021-01-18 13:17:53 -0600

    Working on navbar **Not finished yet**

--- src/services/header/Header.js
+++ src/services/header/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { FormControl, InputGroup, Nav, NavItem } from "react-bootstrap";
+import { Dropdown, Nav, NavItem } from "react-bootstrap";
 import {
   AiOutlineUser,
   AiOutlineSearch,
@@ -19,12 +19,6 @@ class Header extends Component {
       <>
 
       <div class="header">
-        <div class="icons"> 
-          <AiOutlineUser class="left-icon" />
-          <AiOutlineInbox class="left-icon" />
-          <BiMap class="right-icon" />
-        </div>
-
         <Nav navbar className="navBar row col-10 offset-1">
           <NavItem className="navItem col-3 col-sm-3">
             <NavLink className="nav-link" to="#">
@@ -52,27 +46,23 @@ class Header extends Component {
           </NavItem>
         </Nav>
 
-        <div className="row">
-          <InputGroup class="search">
-            <InputGroup.Prepend>
-              <InputGroup.Text id="search">
-                <AiOutlineSearch />
-              </InputGroup.Text>
-            </InputGroup.Prepend>
-            <FormControl
-              placeholder="Search"
-              aria-label="Search"
-              aria-describedby="search"
-            />
-            <InputGroup.Append>
-              <InputGroup.Text>
-                <AiOutlineFilter />
-              </InputGroup.Text>
-            </InputGroup.Append>
-          </InputGroup>
+        <div className="input-group row col-10 col-sm-3 offset-1">
+            <input type="search" id="navSearch" className="form-control" placeholder="Search" 
+             aria-label="Search" aria-describedby="search-addon"/>
         </div>
 
-        
+        <div className="navBottom row col-10 col-sm-10 offset-1">
+          <div className="location col-6 col-sm-6">
+            <span>Location <i class="fa fa-map-marker"></i></span>
+          </div>
+          <div className="col-6 col-sm-6">
+            <select class="form-select form-select-sm" aria-label="Default select example">
+              <option selected>&nbsp;Sort By: Near me </option>
+              <option value="Name">&nbsp;Sort By: Name </option>
+              <option value="Rating">&nbsp;Sort By: Rating </option>
+            </select>
+          </div>
+        </div>   
       </div>
 
       </>
