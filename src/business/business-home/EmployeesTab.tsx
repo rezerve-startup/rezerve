9c88a84ce01057bc33be402b388836666370f37c
Miguel Campbell <72178579+Migotaur@users.noreply.github.com>
2021-06-03 16:00:43 -0500

    User Collection retrieved

--- src/business/business-home/EmployeesTab.tsx
+++ src/business/business-home/EmployeesTab.tsx
@@ -7,15 +7,33 @@ import {
   makeStyles,
   useMediaQuery,
   useTheme,
+  Card,
+  CardActions,
+  CardContent,
+  Typography
 } from '@material-ui/core';
 import { CalendarToday, Description } from '@material-ui/icons';
 import SwipeableViews from 'react-swipeable-views';
 import { firestore } from '../../config/FirebaseConfig';
-
+import firebase from 'firebase'
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
   },
+  employeeList: {
+    display: "flex",
+    padding: "5px",
+    width: "99%",
+    
+  },
+  button: {
+    right: '40px',
+    position: 'fixed'
+  },
+  button2: {
+    left: '40px',
+    position: 'relative'
+  }
 });
 
 export default function EmployeeTab(props: any) {
@@ -24,12 +42,22 @@ export default function EmployeeTab(props: any) {
   const [value, setValue] = React.useState(0);
   const [employees, setEmployees]: any[] = React.useState([])
   const [employeeRequests, setEmployeeRequests]: any[] = React.useState([])
+  const [employeeIds, setEmployeeIds]: any[] = React.useState([])
+  const [employeeReqIds, setEmployeeReqIds]: any[] = React.useState([])
+  const [employeeInfo, setEmployeeInfo]: any[] = React.useState([])
+  const [employeeReqInfo, setEmployeeReqInfo]: any[] = React.useState([])
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   useEffect(() => {
     const businessId = props.businessId
     const tmpEmployees: any = []
     const tmpEmployeeRequests: any = []
+    const tmpEmployeeIds: any = []
+    const tmpEmployeeReqIds: any = []
+    const tmpEmployeeInfo: any = []
+    const tmpEmployeeReqInfo: any = []
+    
+    var tmpUser;
     
     firestore
       .collection('businesses')
@@ -38,29 +66,49 @@ export default function EmployeeTab(props: any) {
       .then((docRef) => {
         const employeeIds: string[] = docRef.data()?.employees
         const employeeReqIds: string[] = docRef.data()?.employeeRequests
+        const userCollection = firestore.collection('users')
 
-        
         firestore
           .collection('employees')
           .onSnapshot((snapshot) => {
             snapshot.docs.forEach((doc) => {
+
               if (employeeIds.includes(doc.id)) {
+                
                 tmpEmployees.push(doc.data())
+                tmpEmployeeIds.push(doc.id)
               } else if (employeeReqIds.includes(doc.id)) {
                 tmpEmployeeRequests.push(doc.data())
+                tmpEmployeeReqIds.push(doc.id)
               }
             })
 
             setEmployees(tmpEmployees);
             setEmployeeRequests(tmpEmployeeRequests);
-          }) 
+            setEmployeeIds(tmpEmployeeIds);
+            setEmployeeReqIds(tmpEmployeeReqIds);
+          })
+          
+          
    
+    }).then(() => {
+      employeeIds.forEach((value) =>{
+        firestore
+        .collection('users').where('employeeId', '==', value )
+        .onSnapshot((snapshot) => {
+          snapshot.docs.forEach((docRef) => {
+              tmpEmployeeInfo.push(docRef.data())
+            })
+            setEmployeeInfo(tmpEmployeeInfo)
+          })
+      })
     })
       
       .catch((e) => {
         console.log(e)
       })
   }, [])
+
 
   const handleChange = (_event: any, newValue: number) => {
     setValue(newValue);
@@ -103,18 +151,62 @@ export default function EmployeeTab(props: any) {
           onChangeIndex={handleChangeIndex}
           enableMouseEvents={true}
         >
+          
           <TabPanel value={value} index={0}>
+            
             {employees.map((emp, index) => {
+            const id = employeeIds[index]
+            
               return(
-                <p key={index}>{emp.position}</p>
+                <div className={classes.employeeList}>
+                  <Card className={classes.employeeList}>
+                    <CardContent>
+                      <div>
+                        <Typography variant="h6">
+                              {id}
+                          </Typography>
+
+                          <Typography variant="subtitle1" color="textSecondary">
+                              {emp.position}
+                          </Typography>
+                      </div>
+                    </CardContent>
+                    <CardActions>
+                      {!emp.isOwner ? (
+                        <button className={classes.button} onClick={() => {removeEmployee(id, props.businessId)}}>Remove</button>
+                      ) : (<span/>)}
+                    </CardActions>
+                  </Card>
+                  </div>
               )
             })
           }
           </TabPanel>
           <TabPanel value={value} index={1}>
           {employeeRequests.map((empR, index) => {
+            const id = employeeReqIds[index]
               return(
-                <p key={index}>Request # {index + 1}---{empR.position}</p>
+                <div className={classes.employeeList}>
+                  <Card className={classes.employeeList}>
+                    <CardContent>
+                      <div>
+                        <Typography variant="h6">
+                              {id}
+                          </Typography>
+
+                          <Typography variant="subtitle1" color="textSecondary">
+                              {empR.position}
+                          </Typography>
+                      </div>
+                    </CardContent>
+                    <CardActions>
+                      <div className={classes.button2}>
+                      <button onClick={() => {acceptRequest(id, props.businessId)}}>Add</button>
+                      <button onClick={() => {removeRequest(id, props.businessId)}}>Remove</button>
+                      </div>
+                    </CardActions>
+                </Card>
+              </div>
               )
             })
           }
@@ -152,4 +244,31 @@ function a11yProps(index: number) {
     id: `appointment-tab-${index}`,
     'aria-controls': `appointment-tabpanel-${index}`,
   };
+}
+
+function acceptRequest(id, businessId){
+    const businessRef = firestore.collection("businesses").doc(businessId);
+    const employeeRef = firestore.collection("employees").doc(id);
+    
+    businessRef.update({
+      employees: firebase.firestore.FieldValue.arrayUnion(id),
+      employeeRequests: firebase.firestore.FieldValue.arrayRemove(id)
+    })
+}
+
+function removeRequest(id, businessId){
+  const businessRef = firestore.collection("businesses").doc(businessId);
+  businessRef.update({
+    employeeRequests: firebase.firestore.FieldValue.arrayRemove(id)
+  })
+}
+
+function removeEmployee(id, businessId){
+  const businessRef = firestore.collection("businesses").doc(businessId);
+  businessRef.update({
+    employees: firebase.firestore.FieldValue.arrayRemove(id)
+  })
+}
+function GetEmployeeInfo(empId){
+
 }
