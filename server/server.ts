10b1e86671cebc715f567be0a91585155a0d6a41
Miguel Campbell <72178579+Migotaur@users.noreply.github.com>
2021-07-17 18:33:50 -0500

    Added cors origin header

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,9 @@ const cors = require('cors');
 
 app.use(express.static('.'));
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {origin: "http://localhost:3000",}
+));
 
 //Accepts payment from previous Setup Intent
 app.post('/create-setup-intent', async (req, res) => {
