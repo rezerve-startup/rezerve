8872f977bf24368c9380790be4b7f3574a3dd371
Miguel Campbell <72178579+Migotaur@users.noreply.github.com>
2021-07-17 14:33:42 -0500

    update

--- server/server.ts
+++ server/server.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(function(req, res, next){
-  res.header("Access-Control-Allow-Origin", "https://rezerve-startup-api.herokuapp.com");
+  res.header("Access-Control-Allow-Origin", "https://rezerve-startup.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next();
 })
